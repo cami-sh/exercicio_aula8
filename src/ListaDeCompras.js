3ce91694a698845bcba327ed1cc4b67a82711600
Camila Sayuri <camilahanazono@gmail.com>
2021-01-20 23:27:25 -0300

    criação da ListaDeCompras e Item

---
+++ src/ListaDeCompras.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Item from './Item';
+
+function ListaDeCompras () {
+    return (
+        <>
+            <Item nome="feijão"/>
+            <Item nome="arroz"/>
+            <Item nome="batata"/>
+            <Item nome="cenoura"/>
+            <Item nome="brócolis"/>
+        </>
+    )
+}
+
+export default ListaDeCompras;
